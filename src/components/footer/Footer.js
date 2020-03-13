96817117d0c2424000b21d12d93706701261b76d
Gerardo Paredes <gerardparedes23@gmail.com>
2020-03-13 12:53:17 -0700

    dynamic year added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import tmdbLogo from '../../assets/img/the-movie-db.png';
 
-const footer = () => (
+const footer = () => {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    return (
     <footer className="footer">
         <a href="https://www.themoviedb.org/?_dc=1547147159" target="_blank" rel="noopener noreferrer">
             <img className="footer__img" src={'https://www.themoviedb.org/assets/1/v4/logos/293x302-powered-by-square-green-3ee4814bb59d8260d51efdd7c124383540fc04ca27d23eaea3a8c87bfa0f388d.png' || tmdbLogo} alt="logo" />
         </a>
         <p className="footer__text">
-            &copy; 2019 FILMANIA 
+            &copy; 
+            {' '}
+            {currentYear}
+            {' '}
+            FILMANIA 
             {' '}
             <br />
             All Rights Reserved.
         </p>
     </footer>
-);
+    );
+};
 
 export default footer;
